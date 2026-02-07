9f52d7cb176954ec01d292858b3b330872b7eebc
Jean Chirinos <jeanchirinos17@gmail.com>
2026-02-07 21:58:47 +0000

    refactor: update getHello method to return JSON representation of data

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,7 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    console.log('Hello World 7!');
+    console.log('Hello World 9!');
 
     const a = {
       a: 1,
